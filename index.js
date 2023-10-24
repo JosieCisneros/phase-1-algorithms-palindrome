4cf70df6a2411f7abd1ba5ae1fd7e7ff9e247c68
Josie <josiecisneros1224@gmail.com>
2023-10-23 18:09:00 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const letters = word.split("")
+  const backwards = letters.reverse().join("").toString()
+
+  if(backwards === word) {
+    return true;
+  } else {
+    return false
+  }
 }
 
+
+
 /* 
-  Add your pseudocode here
+  define a variable (backwards) where the argument (word) is split aprart
+  arguement is then reversed and joined back together in a string
+  if variable is strongly equal to the argument then return true
+  if variable is not strongly equal to argument then return false
 */
 
 /*
-  Add written explanation of your solution here
+  if arguement string is a palindrom return true
+  else artgument string is not a palindrom return false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("mom"))
+
+  console.log("Expecting: false")
+  console.log("=>", isPalindrome("father"))
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
